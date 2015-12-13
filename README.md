3a4d136c97d6c2db8ede65fd3beb78259a95d629
James Aguilar <aguilar.james@gmail.com>
2015-12-13 15:10:48 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#Assembler Light
+
+Adds a small light to the front of active assemblers. This can aid in the
+detection of bottlenecks.
+
+The graphic is super janky. Happy to accept a pull request for a better
+animation.
---
+++ assembler-light_0.0.1/data-updates.lua
@@ -0,0 +1,57 @@
+data.raw["assembling-machine"]["assembling-machine-1"].working_visualisations =
+{
+  {
+    north_position = {0.0, 0.0},
+    east_position = {0.0, 0.0},
+    south_position = {0.0, 0.0},
+    west_position = {0.0, 0.0},
+    animation =
+    {
+      filename = "__assembler-light__/graphics/light.png",
+      priority = "extra-high",
+      width = 12,
+      height = 12,
+      frame_count = 16,
+      line_length = 8,
+      shift = { .50, 1.0 }
+    },
+  }
+};
+data.raw["assembling-machine"]["assembling-machine-2"].working_visualisations =
+{
+  {
+    north_position = {0.0, 0.0},
+    east_position = {0.0, 0.0},
+    south_position = {0.0, 0.0},
+    west_position = {0.0, 0.0},
+    animation =
+    {
+      filename = "__assembler-light__/graphics/light.png",
+      priority = "extra-high",
+      width = 12,
+      height = 12,
+      frame_count = 16,
+      line_length = 8,
+      shift = { .50, 1.0 }
+    },
+  }
+};
+data.raw["assembling-machine"]["assembling-machine-3"].working_visualisations =
+{
+  {
+    north_position = {0.0, 0.0},
+    east_position = {0.0, 0.0},
+    south_position = {0.0, 0.0},
+    west_position = {0.0, 0.0},
+    animation =
+    {
+      filename = "__assembler-light__/graphics/light.png",
+      priority = "extra-high",
+      width = 12,
+      height = 12,
+      frame_count = 16,
+      line_length = 8,
+      shift = { .50, 1.0 }
+    },
+  }
+};
