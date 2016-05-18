d1aa2717da99a3e5da6008a215c1b1d578ea65d4
Jackalope-Gaming <jwhurst13@yahoo.com>
2016-05-18 11:09:50 -0600

    Coding revamp shrinks mod size
    Coding revamp removes redundancies and allows for supporting other mods much more easily if wanted.

--- assembler-light_0.0.2/data-updates.lua
+++ assembler-light_0.0.2/data-updates.lua
@@ -1,57 +1,31 @@
-data.raw["assembling-machine"]["assembling-machine-1"].working_visualisations =
-{
-  {
-    north_position = {0.0, 0.0},
-    east_position = {0.0, 0.0},
-    south_position = {0.0, 0.0},
-    west_position = {0.0, 0.0},
-    animation =
-    {
-      filename = "__assembler-light__/graphics/light.png",
-      priority = "extra-high",
-      width = 12,
-      height = 12,
-      frame_count = 16,
-      line_length = 8,
-      shift = { .75, 1.15 }
-    },
-  }
-};
-data.raw["assembling-machine"]["assembling-machine-2"].working_visualisations =
-{
-  {
-    north_position = {0.0, 0.0},
-    east_position = {0.0, 0.0},
-    south_position = {0.0, 0.0},
-    west_position = {0.0, 0.0},
-    animation =
-    {
-      filename = "__assembler-light__/graphics/light.png",
-      priority = "extra-high",
-      width = 12,
-      height = 12,
-      frame_count = 16,
-      line_length = 8,
-      shift = { .75, 1.15 }
-    },
-  }
-};
-data.raw["assembling-machine"]["assembling-machine-3"].working_visualisations =
-{
-  {
-    north_position = {0.0, 0.0},
-    east_position = {0.0, 0.0},
-    south_position = {0.0, 0.0},
-    west_position = {0.0, 0.0},
-    animation =
-    {
-      filename = "__assembler-light__/graphics/light.png",
-      priority = "extra-high",
-      width = 12,
-      height = 12,
-      frame_count = 16,
-      line_length = 8,
-      shift = { .75, 1.15 }
-    },
-  }
-};
+supported_assemblers = {
+	-- base game assemblers
+	"assembling-machine-1",
+	"assembling-machine-2",
+	"assembling-machine-3",	
+}	
+
+-- reconfigure all supported assemblers to have the light
+for i,name in ipairs(supported_assemblers) do
+	if	data.raw["assembling-machine"][name] then
+		data.raw["assembling-machine"][name].working_visualisations =
+		{
+			{
+			north_position = {0.0, 0.0},
+			east_position = {0.0, 0.0},
+			south_position = {0.0, 0.0},
+			west_position = {0.0, 0.0},
+			animation =
+				{
+				filename = "__assembler-light__/graphics/light.png",
+				priority = "extra-high",
+				width = 12,
+				height = 12,
+				frame_count = 16,
+				line_length = 8,
+				shift = { .75, 1.15 }
+				},
+			}
+		}
+	end
+end
